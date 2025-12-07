6e13a851a92b51759aa757f83717567ac4534b1a
Teun Wolbert <teunw@goed.email>
2025-12-07 21:01:55 +0100

    Remove flicker

--- src/pages/state/store.ts
+++ src/pages/state/store.ts
@@ -2,14 +2,14 @@ import { createStore } from "solid-js/store";
 import { createEffect } from "solid-js";
 
 export const [store, setStore] = createStore({
-	pageTitle: "",
+    pageTitle: "Home",
 });
 
 // Update document.title whenever pageTitle changes
 createEffect(() => {
-	if (store.pageTitle) {
-		document.title = `${store.pageTitle} | Teun Wolbert`;
-	} else {
-		document.title = "Teun Wolbert | Portfolio";
-	}
+    if (store.pageTitle) {
+        document.title = `${store.pageTitle} | Teun Wolbert`;
+    } else {
+        document.title = "Teun Wolbert | Portfolio";
+    }
 });
